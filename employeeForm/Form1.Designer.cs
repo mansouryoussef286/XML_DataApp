4984cb8816e807a5a4792e8d364de5cef60bea8a
Youssef Mansour <mansouryoussef286@gmail.com>
2022-02-08 21:24:25 +0200

    Add files via upload

---
+++ employeeForm/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace employeeForm
+{
+    partial class empForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.phone = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.Label();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.address = new System.Windows.Forms.Label();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.previousBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(46, 39);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(48, 19);
+            this.name.TabIndex = 0;
+            this.name.Text = "name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(117, 41);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(283, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(117, 78);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(283, 20);
+            this.phoneTextBox.TabIndex = 3;
+            // 
+            // phone
+            // 
+            this.phone.AutoSize = true;
+            this.phone.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phone.Location = new System.Drawing.Point(45, 76);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(53, 19);
+            this.phone.TabIndex = 2;
+            this.phone.Text = "phone";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(117, 154);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(283, 20);
+            this.emailTextBox.TabIndex = 7;
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email.Location = new System.Drawing.Point(45, 152);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(47, 19);
+            this.email.TabIndex = 6;
+            this.email.Text = "email";
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(117, 117);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(283, 20);
+            this.addressTextBox.TabIndex = 5;
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address.Location = new System.Drawing.Point(45, 115);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(63, 19);
+            this.address.TabIndex = 4;
+            this.address.Text = "address";
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(421, 39);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(75, 23);
+            this.nextBtn.TabIndex = 8;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // previousBtn
+            // 
+            this.previousBtn.Location = new System.Drawing.Point(421, 76);
+            this.previousBtn.Name = "previousBtn";
+            this.previousBtn.Size = new System.Drawing.Size(75, 23);
+            this.previousBtn.TabIndex = 9;
+            this.previousBtn.Text = "Previous";
+            this.previousBtn.UseVisualStyleBackColor = true;
+            this.previousBtn.Click += new System.EventHandler(this.previousBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(421, 192);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(75, 23);
+            this.updateBtn.TabIndex = 10;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.ForeColor = System.Drawing.Color.Green;
+            this.insertBtn.Location = new System.Drawing.Point(421, 115);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(75, 23);
+            this.insertBtn.TabIndex = 11;
+            this.insertBtn.Text = "Insert";
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.ForeColor = System.Drawing.Color.Red;
+            this.deleteBtn.Location = new System.Drawing.Point(421, 152);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 12;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(117, 195);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(125, 20);
+            this.searchTextBox.TabIndex = 13;
+            this.searchTextBox.Text = "Search by name";
+            this.searchTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.searchTextBox.GotFocus += new System.EventHandler(this.removePlaceHolder);
+            this.searchTextBox.LostFocus += new System.EventHandler(this.addPlaceHolder);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(294, 192);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 14;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // empForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 254);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.previousBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.name);
+            this.Name = "empForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employees";
+            this.Load += new System.EventHandler(this.empForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.Label phone;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label address;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button previousBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchBtn;
+
+    }
+}
+
---
+++ employeeForm/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace employeeForm
+{
+    public partial class empForm : Form
+    {
+        //employees file
+        XmlDocument employeesFile = new XmlDocument();
+        XmlNode employees;
+        //file tags
+        XmlNodeList names;
+        XmlNodeList phones;
+        XmlNodeList addresses;
+        XmlNodeList emails;
+        //file iterator to track the current employee
+        int iterator;
+
+        public empForm()
+        {
+            InitializeComponent();
+        }
+
+        private void empForm_Load(object sender, EventArgs e)
+        {
+            onload();
+        }
+
+        public void onload()
+        {
+            iterator = 0;
+            //load the file
+            employeesFile.Load(@"C:\Users\Blu-Ray\OneDrive\Desktop\iti\XML\lab 1\employee.xml");
+            //get the root element
+            employees = employeesFile.DocumentElement;
+            //get the tag names in array lists
+            names = employeesFile.GetElementsByTagName("name");
+            phones = employeesFile.GetElementsByTagName("phones");
+            addresses = employeesFile.GetElementsByTagName("addresses");
+            emails = employeesFile.GetElementsByTagName("email");
+            //fill the initial data
+            fillData(iterator);
+        }
+        //filling the form data
+        public void fillData(int i)
+        {
+            nameTextBox.Text = names[iterator].InnerText;
+            phoneTextBox.Text = phones[iterator].FirstChild.InnerText;
+            printAddress(i);
+            emailTextBox.Text = emails[iterator].InnerText;
+        }
+        //adding the full address to the textbox
+        private void printAddress(int i)
+        {
+            XmlNodeList address = addresses[i].FirstChild.ChildNodes;
+            string fullAddress = "";
+            foreach (XmlNode node in address)
+            {
+                fullAddress += node.InnerText + ", ";
+            }
+            fullAddress = fullAddress.Remove(fullAddress.Length - 2, 2);
+            fullAddress = fullAddress.Insert(fullAddress.Length, ".");
+            addressTextBox.Text = fullAddress;
+        }
+
+        //next, previous, update, insert, delete and search buttons
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            if (iterator < names.Count - 1)
+            {
+                iterator++;
+            }
+            fillData(iterator);
+        }
+        private void previousBtn_Click(object sender, EventArgs e)
+        {
+            if (iterator != 0)
+            {
+                iterator--;
+            }
+            fillData(iterator);
+        }
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+            //the last index of the added element
+            int index = names.Count;
+            //create an employee element cloned from current node
+            //employee = employeesFile.GetElementsByTagName("employee")[0].CloneNode(true);
+
+            XmlNode employee = createEmployeeElement();
+            //append the new node to the file
+            employees.AppendChild(employee);
+            //assign the node values
+            names[index].InnerText = nameTextBox.Text;
+            phones[index].FirstChild.InnerText = phoneTextBox.Text;
+            fillAddressElement(addressTextBox.Text, index);
+            emails[index].InnerText = emailTextBox.Text;
+            employeesFile.Save(@"C:\Users\Blu-Ray\OneDrive\Desktop\iti\XML\lab 1\employee.xml");
+        }
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            //get the current node
+            XmlNode currentNode = employeesFile.GetElementsByTagName("employee")[iterator];
+            employees.RemoveChild(currentNode);
+            employeesFile.Save(@"C:\Users\Blu-Ray\OneDrive\Desktop\iti\XML\lab 1\employee.xml");
+            onload();
+        }
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            names[iterator].InnerText = nameTextBox.Text;
+            phones[iterator].FirstChild.InnerText = phoneTextBox.Text;
+            emails[iterator].InnerText = emailTextBox.Text;
+            fillAddressElement(addressTextBox.Text, iterator);
+            employeesFile.Save(@"C:\Users\Blu-Ray\OneDrive\Desktop\iti\XML\lab 1\employee.xml");
+        }
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            bool Found = false;
+            for (int i = 0; i < names.Count && !Found; i++)
+            {
+                if (string.Compare(names[i].InnerText, searchTextBox.Text, true) == 0)
+                {
+                    iterator = i;
+                    fillData(i);
+                    Found = true;
+                }
+            }
+            if (!Found)
+                MessageBox.Show("this name doesnot exist");
+        }
+
+
+        //creating an employee element with its sub nodes
+        public XmlElement createEmployeeElement()
+        {
+            //employee element
+            XmlNode employee = employeesFile.CreateElement("employee");
+            //name element
+            XmlNode name = employeesFile.CreateElement("name");
+            //phones element
+            XmlNode phones = employeesFile.CreateElement("phones");
+            XmlNode phone = employeesFile.CreateElement("phone");
+            XmlAttribute type = employeesFile.CreateAttribute("type");
+            type.Value = "mobile";
+            phone.Attributes.Append(type);
+            //addresses element
+            XmlNode addresses = employeesFile.CreateElement("addresses");
+            XmlNode address = employeesFile.CreateElement("address");
+            XmlNode street = employeesFile.CreateElement("street");
+            XmlNode buildingNumber = employeesFile.CreateElement("buildingNumber");
+            XmlNode region = employeesFile.CreateElement("region");
+            XmlNode city = employeesFile.CreateElement("city");
+            XmlNode country = employeesFile.CreateElement("country");
+            //email element
+            XmlNode email = employeesFile.CreateElement("email");
+
+            //append these elements
+            phones.AppendChild(phone);
+            address.AppendChild(street);
+            address.AppendChild(buildingNumber);
+            address.AppendChild(region);
+            address.AppendChild(city);
+            address.AppendChild(country);
+            addresses.AppendChild(address);
+            employee.AppendChild(name);
+            employee.AppendChild(phones);
+            employee.AppendChild(addresses);
+            employee.AppendChild(email);
+
+            return (XmlElement)employee;
+        }
+        //parsing the address
+        public void fillAddressElement(string addressText, int i)
+        {
+            string[] add = addressText.Split(',');
+            XmlNode addressElement = addresses[i].FirstChild;
+            XmlNodeList addressElements = addressElement.ChildNodes;
+            //assigning the address elements from the parsed string of form address
+            addressElements[0].InnerText = add[0];
+            for (int j = 1; j < 4; j++)
+            {
+                addressElements[j].InnerText = add[j].Remove(0, 1);
+            }
+            addressElements[4].InnerText = add[4].Remove(add[4].Length - 1, 1).Remove(0, 1);
+        }
+
+
+        //adding placeholder to the search box
+        private void removePlaceHolder(object sender, EventArgs e)
+        {
+            if (searchTextBox.Text == "Search by name")
+            {
+                searchTextBox.Text = "";
+
+            }
+        }
+        private void addPlaceHolder(object sender, EventArgs e)
+        {
+            if (searchTextBox.Text == "")
+                searchTextBox.Text = "Search by name";
+        }
+    }
+}
